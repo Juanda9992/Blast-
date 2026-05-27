52aeb7785ead3774a218f2bc1189e97be1692b64
Juanda999 <juanda999games@gmail.com>
2026-05-27 00:53:37 -0500

    Add canon drag creation

--- Assets/Scripts/Canon/CanonGeneratorManager.cs
+++ Assets/Scripts/Canon/CanonGeneratorManager.cs
@@ -2,14 +2,17 @@ using UnityEngine;
 
 public class CanonGeneratorManager : MonoBehaviour
 {
-    [SerializeField] private Transform canonGenerationParent;
-    [SerializeField] private GameObject canonPlatformPrefab;
-
+    [Header("Positioning Settings")]
+    [SerializeField] private Transform canonSlotsParent;
+    [SerializeField] private Transform canonDragParent;
     [SerializeField] private int cannonsToCreate;
 
     [SerializeField] private float avaliableSpace = 6;
-
-    [SerializeField] private float canonGeneratorParentZPos;
+    [SerializeField] private float canonSlotParentZPos;
+    [SerializeField] private float canonDragParentZPos;
+    [Header("Prefab Settings")]
+    [SerializeField] private GameObject canonPlatformPrefab;
+    [SerializeField] private GameObject canonDragPrefab;
 
     private void Start()
     {
@@ -21,11 +24,15 @@ public class CanonGeneratorManager : MonoBehaviour
 
         for (int i = 0; i < cannonsToCreate; i++)
         {
-            GameObject createdObjects = Instantiate(canonPlatformPrefab, canonGenerationParent);
+            GameObject canonSlot = Instantiate(canonPlatformPrefab, canonSlotsParent);
+            canonSlot.transform.localPosition = new Vector3((spaceBetweenObjects * i) - offset, 0, 0);
 
-            createdObjects.transform.localPosition = new Vector3((spaceBetweenObjects * i) - offset, 0, 0);
+
+            GameObject canonDrag = Instantiate(canonDragPrefab, canonDragParent);
+            canonDrag.transform.localPosition = new Vector3((spaceBetweenObjects * i) - offset, 0, 0);
         }
 
-        canonGenerationParent.transform.position = new Vector3(0,0,canonGeneratorParentZPos);
+        canonSlotsParent.transform.position = new Vector3(0,0,canonSlotParentZPos);
+        canonDragParent.transform.position = new Vector3(0,0,canonDragParentZPos);
     }
 }
