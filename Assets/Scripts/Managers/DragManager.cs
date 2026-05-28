c14635cf294c44d80c9932ad6f63d0f30afa76d7
Juanda999 <juanda999games@gmail.com>
2026-05-28 00:06:16 -0500

    Add logic for draggfing canon

---
+++ Assets/Scripts/Managers/DragManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DragManager : MonoBehaviour
+{
+    [SerializeField] private InputActionReference mousePos, mouseDown;
+    [SerializeField] private Camera _camera;
+    private CanonDrag currentCanon = null;
+    void Awake()
+    {
+        mousePos.action.Enable();
+        mouseDown.action.Enable();
+    }
+    void Update()
+    {
+
+        if(mouseDown.action.WasPressedThisFrame())
+        {
+            Ray ray = _camera.ScreenPointToRay(mousePos.action.ReadValue<Vector2>());
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit,50))
+            {
+                hit.collider.TryGetComponent<CanonDrag>(out currentCanon);
+            }
+        }
+
+        if(mouseDown.action.WasReleasedThisFrame())
+        {
+            currentCanon =null;
+        }
+
+        HandleCanonMovement();
+    }
+    private void HandleCanonMovement()
+    {
+        if(currentCanon == null)
+        {
+            return;
+        }
+
+        Vector3 worldPos = _camera.ScreenToWorldPoint(mousePos.action.ReadValue<Vector2>());
+        worldPos.y = 0;
+        currentCanon.transform.position = worldPos;
+    }
+}
