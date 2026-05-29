0ab8b3cac57bde55f077c8ff039ccfe9427b2f8d
Juanda999 <juanda999games@gmail.com>
2026-05-29 00:43:42 -0500

    Add logic to spawn centered blocks

--- Assets/Scripts/Obstacle/ObstacleSpawner.cs
+++ Assets/Scripts/Obstacle/ObstacleSpawner.cs
@@ -20,15 +20,17 @@ public class ObstacleSpawner : MonoBehaviour
     private IEnumerator SpawnBlocksCoroutine()
     {
         SOLevelRules rules = levelRulesManager.GetLevelRules();
-
+        float blockXSize = 0.9f;
+        float totalWidth = blockXSize * 10;
+        float offset = totalWidth / 2;
         for(int i = 0; i< rules.rows.Count;i++)
         {
             for(int j = 0; j<rules.rows[i].blocks.Length;j++)
             {
                 ObstacleBehaviour obstacle = Instantiate(obstaclePrefab,spawnerParent);
 
-                float xPos = startXCoor + (j+0.5f);
-                obstacle.transform.localPosition = new Vector3(startXCoor + (0.9f*j),0,startYCoor+i);
+                float xPos = (j * blockXSize) - offset + (blockXSize / 2);
+                obstacle.transform.localPosition = new Vector3(xPos,0,startYCoor+i);
                 obstacle.SetUpBlock(rules.rows[i].blocks[j]);
             }
             yield return null;
